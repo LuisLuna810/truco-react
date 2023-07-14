a6321fbdc3d196a3177d6d61849f81da9f834ea6
LuisLuna810 <luis.agustin10@hotmail.com>
2023-07-14 01:01:52 -0300

    fix

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -16,7 +16,7 @@ export function Contador() {
 
   return (
     <div className="container">
-      <div className="mt-12 h-52 text-8xl sm-pb md-pb">{puntos}</div>
+      <div className="mt-12 h-52 text-8xl">{puntos}</div>
       <div className><Botones handleIncrement={handleIncrement} handleDecrement={handleDecrement}/></div>
     </div>
   );
